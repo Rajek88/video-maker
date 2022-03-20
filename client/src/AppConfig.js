bc6dadbf23b64c9f7d12ddc7ce418ed57206e60d
Rajendra Kulkarni <rajerkulkarni01@gmail.com>
2022-03-20 22:46:48 +0530

    for origin v1

--- client/src/AppConfig.js
+++ client/src/AppConfig.js
@@ -1,1 +1,1 @@
-export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "https://img2video.herokuapp.com";
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = 8000;
+const port = process.env.PORT || 8000;
 const app = express();
 
 // middleware
